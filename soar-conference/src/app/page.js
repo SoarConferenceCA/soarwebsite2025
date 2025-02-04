dc5d7aaaf3ed357e17e4cdc3daccccefa17cad5c
sanat-g <sanatguduru387@gmail.com>
2025-02-03 20:47:16 -0500

    initial commit

--- soar-conference/src/app/page.js
+++ soar-conference/src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import {Navbar} from "@/components/navbar";
 import classNames from "classnames";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
@@ -11,54 +12,9 @@ export default function Home() {
         <img src="/soar_logo.png" className="z-10 mx-auto place-self-center text-right mt--12 lg:mt-0"/>
         <div className="w-full h-full absolute bg-black opacity-65"></div>
       </header>
+
+      <Footer />
       
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
